12026f5d5c9f2e2a85c4204ec79fcb57240e3778
khaoula <bouzartikhaoula@gmail.com>
2024-08-07 12:05:48 +0200

    animation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
   AOS.init();
+
+
+  // onclick="scrollToSection()"
+
+  function scrollToServices() {
+    const section = document.getElementById('services');
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+
+function  scrollToContact() {
+  const section = document.getElementById('contact');
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAbout() {
+  const section = document.getElementById('about');
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+function Home() {
+  const section = document.getElementById('home');
+    section.scrollIntoView({ behavior: 'smooth' });
+}
